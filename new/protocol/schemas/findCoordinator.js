332e3b2ff927edb03739e37231103c2ffa66bd78
mpipet <contact@mathieupipet.fr>
2017-12-30 13:39:08 +0100

    update protocole schema

--- new/protocol/schemas/findCoordinator.js
+++ new/protocol/schemas/findCoordinator.js
@@ -1,17 +1,28 @@
 module.exports = {
-    request: {
-        0: [
-            ['group_id', 'string']
-
-        ]
-    },
-    response: {
-        0: [
-            ['error_code', 'int16'],
-            ['node_id', 'int32'],
-            ['host', 'string'],
-            ['port', 'int32']
-        ]
-    }
+  request: {
+    0: [
+      ['group_id', 'string']
+    ],
+    1: [
+      ['group_id', 'string'],
+      ['group_type', 'int8'],
+    ]
+  },
+  response: {
+    0: [
+      ['error_code', 'int16'],
+      ['node_id', 'int32'],
+      ['host', 'string'],
+      ['port', 'int32']
+    ],
+    1: [
+      ['throttle_time_ms', '32'],
+      ['error_code', 'int16'],
+      ['error_message', 'string'],
+      ['node_id', 'int32'],
+      ['host', 'string'],
+      ['port', 'int32']
+    ]
+  }
 
 };
---
+++ new/protocol/schemas/metadata.js
@@ -0,0 +1,221 @@
+const metadataSchema = {
+  request: {
+    0: [
+      [
+        'topics',
+        ['Array', 'string']
+      ]
+    ],
+    4: [
+      [
+        'topics',
+        ['Array', 'string'],
+        ['allow_auto_topic_creation', 'boolean'],
+      ]
+    ]
+  },
+  response: {
+    0: [
+      [
+        'brokers',
+        [
+          'Array',
+          [
+            ['node_id', 'int32'],
+            ['host', 'string'],
+            ['port', 'int32'],
+          ]
+        ]
+      ],
+      ['cluster_id', 'string'],
+      ['controller_id', 'int32'],
+      [
+        'topic_metadata',
+        [
+          'Array',
+          [
+            ['error_code', 'int16'],
+            ['topic', 'string'],
+            [
+              'partition_metadata',
+              [
+                'Array',
+                [
+                  ['error_code', 'int16'],
+                  ['partition', 'int32'],
+                  ['leader', 'int32'],
+                  [
+                    'replicas', [
+                      'Array', 'int32'
+                    ]
+                  ],
+                  [
+                    'isr', [
+                      'Array', 'int32'
+                    ]
+                  ]
+                ] 
+              ] 
+            ]
+          ]
+        ]
+      ]
+    ],
+    1: [
+      [
+        'brokers',
+        [
+          'Array',
+          [
+            ['node_id', 'int32'],
+            ['host', 'string'],
+            ['port', 'int32'],
+          ]
+        ]
+      ],
+      ['cluster_id', 'string'],
+      ['controller_id', 'int32'],
+      [
+        'topic_metadata',
+        [
+          'Array',
+          [
+            ['error_code', 'int16'],
+            ['topic', 'string'],
+            [
+              'partition_metadata',
+              [
+                'Array',
+                [
+                  ['error_code', 'int16'],
+                  ['partition', 'int32'],
+                  ['leader', 'int32'],
+                  [
+                    'replicas', [
+                      'Array', 'int32'
+                    ]
+                  ],
+                  [
+                    'isr', [
+                      'Array', 'int32'
+                    ]
+                  ]
+                ] 
+              ] 
+            ]
+          ]
+        ]
+      ]
+    ],
+    3: [
+      ['throttle_time_ms', 'int32'],
+      [
+        'brokers',
+        [
+          'Array',
+          [
+            ['node_id', 'int32'],
+            ['host', 'string'],
+            ['port', 'int32'],
+          ]
+        ]
+      ],
+      ['cluster_id', 'string'],
+      ['controller_id', 'int32'],
+      [
+        'topic_metadata',
+        [
+          'Array',
+          [
+            ['error_code', 'int16'],
+            ['topic', 'string'],
+            [
+              'partition_metadata',
+              [
+                'Array',
+                [
+                  ['error_code', 'int16'],
+                  ['partition', 'int32'],
+                  ['leader', 'int32'],
+                  [
+                    'replicas', [
+                      'Array', 'int32'
+                    ]
+                  ],
+                  [
+                    'isr', [
+                      'Array', 'int32'
+                    ]
+                  ]
+                ] 
+              ] 
+            ]
+          ]
+        ]
+      ]
+    ],
+    5: [
+      ['throttle_time_ms', 'int32'],
+      [
+        'brokers',
+        [
+          'Array',
+          [
+            ['node_id', 'int32'],
+            ['host', 'string'],
+            ['port', 'int32'],
+          ]
+        ]
+      ],
+      ['cluster_id', 'string'],
+      ['controller_id', 'int32'],
+      [
+        'topic_metadata',
+        [
+          'Array',
+          [
+            ['error_code', 'int16'],
+            ['topic', 'string'],
+            [
+              'partition_metadata',
+              [
+                'Array',
+                [
+                  ['error_code', 'int16'],
+                  ['partition', 'int32'],
+                  ['leader', 'int32'],
+                  [
+                    'replicas', [
+                      'Array', 'int32'
+                    ]
+                  ],
+                  [
+                    'isr', [
+                      'Array', 'int32'
+                    ]
+                  ],
+                  ['offline_replicas', 'int32']
+                ] 
+              ] 
+            ]
+          ]
+        ]
+      ]
+    ]         
+  }
+  
+};
+
+metadataSchema.request[1] = metadataSchema.request[0];
+metadataSchema.request[2] = metadataSchema.request[0];
+metadataSchema.request[3] = metadataSchema.request[0];
+
+metadataSchema.request[5] = metadataSchema.request[4];
+
+metadataSchema.response[2] = metadataSchema.request[1];
+
+metadataSchema.response[4] = metadataSchema.request[3];
+
+
+
+module.exports = metadataSchema;
